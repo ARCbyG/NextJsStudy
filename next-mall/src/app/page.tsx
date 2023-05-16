806890fd5ad9733f7321bea8b4840f667019f1ea
김혜정 <ARCbyG@miksystem.com>
2023-05-17 06:18:11 +0900

    component 변경 시 특정 동작하기

--- next-mall/src/app/page.tsx
+++ next-mall/src/app/page.tsx
@@ -1,19 +1,28 @@
 'use client'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
   // 초기값 설정
   const [x, setX] = useState(0);
 
+
   // count 변경
   function handleCountChange(addCount: number) {
     setX(x + addCount)
   }
 
+  // x 변경
+  useEffect(() => {
+    if (x > 10 || x < -10) {
+      setX(0)
+    }
+    else {
+      console.log(x)
+    }
+  })
 
-  
 
   // const [person, setPerson] = useState({
   //   firstName: 'Barbara',
@@ -48,13 +57,13 @@ export default function Home() {
         Count: {x}
       </div>
       <div>
-      <button onClick={(e) => handleCountChange(1)}>증가</button>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-      <button onClick={(e) => handleCountChange(-1)}>감소</button>
+        <button onClick={(e) => handleCountChange(1)}>증가</button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <button onClick={(e) => handleCountChange(-1)}>감소</button>
       </div>
-      
 
-{/*       
+
+      {/*       
       <label>
         First name:
         <input
