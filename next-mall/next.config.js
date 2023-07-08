6e536f6d9636ca1018908a20cbeff9913d7c50b9
김혜정 <ARCbyG@gmail.com>
2023-07-08 16:29:25 +0900

    기본 페이지 경로 변경

--- next-mall/next.config.js
+++ next-mall/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects(){
+        return [
+            {
+                // 들어오는 요청경로
+                source:"/",
+
+                // 라우팅 하려는 경로
+                // destination:'/page/main',
+                destination:'/main',
+
+                // 브라우저나 검색엔진이 이 정보를 기억할지 여부
+                permanent: true,
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
